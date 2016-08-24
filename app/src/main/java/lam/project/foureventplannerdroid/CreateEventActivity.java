de2174a1cc81f55c330021de0b978635b6cc549a
Valentina <94tinat@gmail.com>
2016-08-24 20:04:26 +0200

    Aggiunto caricamento immagine nella creazione dell'evento

--- app/src/main/java/lam/project/foureventplannerdroid/CreateEventActivity.java
+++ app/src/main/java/lam/project/foureventplannerdroid/CreateEventActivity.java
@@ -1,7 +1,19 @@
 package lam.project.foureventplannerdroid;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -24,12 +36,18 @@ import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Calendar;
 
 import lam.project.foureventplannerdroid.model.Category;
 import lam.project.foureventplannerdroid.model.Event;
 import lam.project.foureventplannerdroid.utils.CustomRequest;
 import lam.project.foureventplannerdroid.utils.FourEventUri;
+import lam.project.foureventplannerdroid.utils.Utility;
 import lam.project.foureventplannerdroid.utils.VolleyRequest;
 
 public class CreateEventActivity extends AppCompatActivity implements View.OnClickListener,
@@ -50,6 +68,11 @@ public class CreateEventActivity extends AppCompatActivity implements View.OnCli
     private TextView endTime;
     private ImageView imgEvent;
 
+    private String mImageUri;
+
+    private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
+    private String userChoosenTask;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +89,13 @@ public class CreateEventActivity extends AppCompatActivity implements View.OnCli
         endDate = (TextView) findViewById(R.id.event_end_date);
         endTime = (TextView) findViewById(R.id.event_end_time);
 
+        imgEvent = (ImageView) findViewById(R.id.event_image);
+        imgEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
 
         startDate.addTextChangedListener(watcher);
         startTime.addTextChangedListener(watcher);
@@ -138,7 +168,6 @@ public class CreateEventActivity extends AppCompatActivity implements View.OnCli
         mAddress = ((TextView)(findViewById(R.id.event_address))).getText().toString();
         mDescription = ((TextView)(findViewById(R.id.event_description))).getText().toString();
         mStartDate = ((TextView)(findViewById(R.id.event_start_date))).getText().toString();
-        imgEvent = ((ImageView)(findViewById(R.id.event_image)));
 
         //TODO completare
         String uri = FourEventUri.Builder.create(FourEventUri.Keys.EVENT)
@@ -151,13 +180,9 @@ public class CreateEventActivity extends AppCompatActivity implements View.OnCli
     }
 
     private final TextWatcher watcher = new TextWatcher() {
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
-        }
-
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-        }
+        public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
         public void afterTextChanged(Editable s) {
             startTime.setVisibility(View.VISIBLE);
@@ -187,7 +212,6 @@ public class CreateEventActivity extends AppCompatActivity implements View.OnCli
             }
         }
     };
-
 
     @Override
     public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int second) {
@@ -243,4 +267,113 @@ public class CreateEventActivity extends AppCompatActivity implements View.OnCli
         });
         tpd.show(getFragmentManager(), "Timepickerdialog");
     }
+
+    private void selectImage() {
+
+        final CharSequence[] items = { "Scatta una foto", "Scegli dalla galleria", "Annulla" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(CreateEventActivity.this);
+        builder.setTitle("Aggiungi un'immagine");
+
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result = Utility.checkPermission(CreateEventActivity.this);
+                if (items[item].equals("Scatta una foto")) {
+                    userChoosenTask = "Scatta una foto";
+                    if(result)
+                        cameraIntent();
+
+                } else if (items[item].equals("Scegli dalla galleria")) {
+                    userChoosenTask = "Scegli dalla galleria";
+                    if(result)
+                        galleryIntent();
+
+                } else if (items[item].equals("Annulla")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void cameraIntent()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    private void galleryIntent()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case Utility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if(userChoosenTask.equals("Take Photo"))
+                        cameraIntent();
+                    else if(userChoosenTask.equals("Choose from Library"))
+                        galleryIntent();
+                } else {
+                    //Codice per negare i permessi
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCaptureImageResult(data);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+        Bitmap bm = null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        imgEvent.setImageBitmap(bm);
+    }
+
+    private void onCaptureImageResult(Intent data) {
+
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+
+        mImageUri = System.currentTimeMillis() + ".jpg";
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                mImageUri);
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        imgEvent.setImageBitmap(thumbnail);
+    }
+
 }
