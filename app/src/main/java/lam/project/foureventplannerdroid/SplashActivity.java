d60b913964cd305c06de06ee04f089fe17c91196
Valentina <94tinat@gmail.com>
2016-08-27 16:42:53 +0200

    Inserito splash

---
+++ app/src/main/java/lam/project/foureventplannerdroid/SplashActivity.java
@@ -0,0 +1,71 @@
+package lam.project.foureventplannerdroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long MIN_WAIT_INTERVAL = 1500L;
+    private static final long MAX_WAIT_INTERVAL = 3000L;
+    private static final int GO_AHEAD_WHAT = 1;
+
+    private long mStartTime;
+    private boolean mIsDone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        final ImageView logoImageView = (ImageView) findViewById(R.id.splash_imageview);
+        assert logoImageView != null;
+        logoImageView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                long elapsedTime = SystemClock.uptimeMillis() - mStartTime;
+                if(elapsedTime >= MIN_WAIT_INTERVAL){
+
+                    goAhead();
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void goAhead() {
+
+       Intent intent;
+       /* User user = UserManager.get(this).getUser();
+
+        //se l'utente online non c'è
+        if(user == null ){
+
+            StepManager.get(this).setStep(StepManager.INCOMPLETE);
+            intent = new Intent(this,RegistrationActivity.class);
+
+        } else {*/
+
+            intent = new Intent(this,MainActivity.class);
+       // }
+
+        startActivity(intent);
+        finish();
+    }
+}
